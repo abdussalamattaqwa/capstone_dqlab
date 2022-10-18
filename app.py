cb20f0eeb3924e97fbb312342ed6ed04b04fad4c
abdussalamattaqwa <salamabdus072@gmail.com>
2022-10-18 22:27:59 +0800

    Processing data mean-years-of-schooling

--- app.py
+++ app.py
@@ -1,16 +1,47 @@
 import streamlit as st
 import pandas as pd
+import altair as alt
 
 st.title("Korelasi rata-rata lama sekolah dan pengangguran di Indonesia")
 
-st.write("Pengangguran sering dikaitkan dengan pendidikan seseorang, apakah pendidikan atau lama sekolah mempengaruhi jumlah pengangguran di Indonesia?")
+
+st.write("Penelitian Polla (2021) mengungkapkan Pendidikan berpengaruh terhadap pengangguran. Pendidikan diposisikan sebagai sarana untuk peningkatan kesejahteraan melalui pemanfaatan kesempatan kerja yang ada dan mencerminkan tingkat kepandaian dan pencapaian pendidikan formal dari penduduk karena semakin tinggi pula kemampuan kerja atau produktivitas seseorang dalam bekerja. Pendidikan yang tinggi dapat mempengaruhi banyak sektor salah satunya ialah tingkat pengangguran. Jadi apakah Pendidikan dalam hal ini Rata rata lama sekolah memiliki korelasi dengan tingkat pengangguran di Indonesia?")
+
 df = pd.read_csv("Presentase pengangguran diindonesia.csv")
-st.dataframe(df)
+st.dataframe(df.style.format({'Rata rata lama sekolah (Tahun)': '{:.2f}', 
+                             'Presentase pengangguran (%)': '{:.2f}',
+                             'Presentase bekerja(%)' : '{:.2f}'}))
 
-st.caption("Data diatas menunjukkan data rata rata lama sekolah dan pengangguran di indonesia yang diambil dari Badan Pusat Statistik Indonesia. Data yang tersedia adalah data dari 9 tahun terakhir.")
 
-# st.line_chart(df)
-st.line_chart(data=df,x='tahun', y=['Rata rata lama sekolah (Tahun)', 'Presentase pengangguran (%)'])
-st.write("Grafik diatas menunjukkan rata rata lama sekolah di indonesia meningkat secara fluktuasif dari tahun ke tahun. Peningkatan tidak terjadi secara signifikan selama sembilan tahun terakhir. Data dari tahun 2013 hingga 2021 hanya meningkat sebanyak 0,70% dari awalnya 7,61 tahun ke 8,54 tahun. Sedangkan presentase pengangguran mengalami fluktuasi selama sembilan tahun terakhir.")
 
-st.subheader("Rata-rata lama sekolah di Indonesia tidak terlalu mempengaruhi jumlah pengangguran diindonesia, namun rata-rata lama sekolah dapat mengurangi jumlah pengangguran meskipun tidak secara signifikan")
+st.caption("Badan Pusat Statistik merilis Rata-rata Lama Sekolah (RLS) dan Persentase Penduduk yang Pengangguran di Indonesia periode 2013 hingga 2021.")
+
+
+
+new_df1 = df[['tahun','Rata rata lama sekolah (Tahun)']]
+new_df1 = new_df1.rename(columns={'Rata rata lama sekolah (Tahun)': 'value'})
+new_df1 = new_df1.assign(label = 'Rata rata lama sekolah (Tahun)')
+
+new_df2 = df[['tahun','Presentase pengangguran (%)']]
+new_df2 = new_df2.rename(columns={'Presentase pengangguran (%)': 'value'})
+new_df2 = new_df2.assign(label = 'Presentase pengangguran (%)')
+new_df = pd.concat([new_df1, new_df2])
+
+new_df['dates'] = pd.to_datetime(new_df['tahun'], format='%Y')
+
+chart = alt.Chart(new_df).mark_line(point = True).encode(
+    x = alt.X("dates", title="Year"),
+    y="value",
+    color='label',
+).properties(
+    title="Grafik Rata-rata Lama Sekolah (Tahun) dan Persentase Pengangguran (%)"
+)
+st.altair_chart(chart, use_container_width=True)
+
+st.write("Grafik diatas menunjukkan rata rata lama sekolah di indonesia cenderung stabil selama 9 tahun terakhir. Hal tersebut tercermin membaiknya rata-rata lama sekolah di Indonesia. Data dari tahun 2013 hingga 2021 hanya meningkat sebanyak 0,70% dari awalnya 7,61 tahun ke 8,54 tahun. Sedangkan persentase pengangguran mengalami fluktuasi selama sembilan tahun terakhir. Tingkat perubahan signifikan persentase pengangguran terjadi pada tahun 2019 hingga 2021 yang telah diketahui padaa periode tersebut merupakan awal dari munculnya COVID-19.")
+
+
+st.subheader("Kesimpulan")
+st.markdown("1. Rata-rata lama sekolah di Indonesia tidak mempengaruhi persentase pengangguran diindonesia.")
+st.markdown("2. Rata-rata lama sekolah meningkat cenderung stabil dari tahun ke tahun")
+st.markdown("3. Persentase pengangguran meningkat secara signifikan saat munculnya wabah COVID-19.")
