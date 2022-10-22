3dee606d0c54ce54c237abf3569585f9c0600cde
abdussalamattaqwa <salamabdus072@gmail.com>
2022-10-23 07:24:48 +0800

    Final submission capstone project DQLab

--- app.py
+++ app.py
@@ -31,11 +31,12 @@ chart = alt.Chart(final_top).mark_line(point=True).encode(
     title="Persentase Perbandingan Rata-rata Lama Sekolah di Indonesia dengan Top 5 Negara Lain"
 )
 st.altair_chart(chart, use_container_width=True)
-st.write("Rata rata lama sekolah di Indonesia dibandingkan dengan negara lain sangat jauh. German sebagai negara tertinggi dengan rata-rata lama sekolah pada tahun 2019 berada pada angka 14,13 tahun yang mengartikan rata-rata penduduk German telah menempuh pendidikan selama 14,13 tahun. Hasil ini sangat jauh dibandingkan Indonesia yang pada tahun 2019 mencapai 8,34 tahun selisih 5,79 tahun dengan rata-rata lama sekolah di German.")
+st.write("Rata rata lama sekolah di Indonesia dibandingkan dengan negara lain sangat jauh. German sebagai negara tertinggi dengan rata-rata lama sekolah pada tahun 2019 berada pada angka 14,13 tahun yang mengartikan rata-rata penduduk German telah menempuh pendidikan selama 14,13 tahun. Hasil ini sangat jauh dibandingkan Indonesia yang pada tahun 2019 mencapai 8,34 tahun selisih 5,79 tahun dengan rata-rata lama sekolah di German. Perkembangan rata-rata lama sekolah di Indonesia dari tahun ke tahun juga termasuk rendah dibandingkan dengan negara lain.")
 
 st.subheader(
     'Rata-rata Lama Sekolah dan Persentase Penangguran di Indonesia')
 df = pd.read_csv("Presentase pengangguran diindonesia.csv")
+df['dates'] = pd.to_datetime(df['tahun'], format='%Y')
 st.dataframe(df.style.format({'Rata rata lama sekolah (Tahun)': '{:.2f}',
                              'Presentase pengangguran (%)': '{:.2f}',
                               'Presentase bekerja(%)': '{:.2f}'}))
@@ -44,40 +45,57 @@ st.dataframe(df.style.format({'Rata rata lama sekolah (Tahun)': '{:.2f}',
 st.caption("Badan Pusat Statistik merilis Rata-rata Lama Sekolah (RLS) dan Persentase Penduduk yang Pengangguran di Indonesia periode 2013 hingga 2021.")
 
 
-new_df1 = df[['tahun', 'Rata rata lama sekolah (Tahun)']]
-new_df1 = new_df1.rename(columns={'Rata rata lama sekolah (Tahun)': 'value'})
-new_df1 = new_df1.assign(label='Rata rata lama sekolah (Tahun)')
+# new_df1 = df[['tahun', 'Rata rata lama sekolah (Tahun)']]
+# new_df1 = new_df1.rename(columns={'Rata rata lama sekolah (Tahun)': 'value'})
+# new_df1 = new_df1.assign(label='Rata rata lama sekolah (Tahun)')
 
-new_df2 = df[['tahun', 'Presentase pengangguran (%)']]
-new_df2 = new_df2.rename(columns={'Presentase pengangguran (%)': 'value'})
-new_df2 = new_df2.assign(label='Presentase pengangguran (%)')
-new_df = pd.concat([new_df1, new_df2])
+# new_df2 = df[['tahun', 'Presentase pengangguran (%)']]
+# new_df2 = new_df2.rename(columns={'Presentase pengangguran (%)': 'value'})
+# new_df2 = new_df2.assign(label='Presentase pengangguran (%)')
+# new_df = pd.concat([new_df1, new_df2])
 
-new_df['dates'] = pd.to_datetime(new_df['tahun'], format='%Y')
+# new_df['dates'] = pd.to_datetime(new_df['tahun'], format='%Y')
 
-chart = alt.Chart(new_df).mark_line(point=True).encode(
-    x=alt.X("dates", title="Year"),
-    y="value",
-    color='label',
+
+chart_rata_rata_lama_sekolah = alt.Chart(df).mark_line(point=True).encode(
+    x=alt.X("dates", title="Tahun", axis=alt.Axis(format='%Y')),
+    y="Rata rata lama sekolah (Tahun)"
 ).properties(
-    title="Grafik Rata-rata Lama Sekolah (Tahun) dan Persentase Pengangguran (%)"
+    title="Grafik Rata-rata Lama Sekolah (Tahun)"
 )
-st.altair_chart(chart, use_container_width=True)
 
-st.write("Grafik diatas menunjukkan rata rata lama sekolah di indonesia cenderung stabil selama 9 tahun terakhir. Hal tersebut tercermin membaiknya rata-rata lama sekolah di Indonesia. Data dari tahun 2013 hingga 2021 hanya meningkat sebanyak 0,70% dari awalnya 7,61 tahun ke 8,54 tahun. Sedangkan persentase pengangguran mengalami fluktuasi selama sembilan tahun terakhir. Tingkat perubahan signifikan persentase pengangguran terjadi pada tahun 2020 hingga 2021 yang telah diketahui pada periode tersebut merupakan awal dari munculnya COVID-19.")
+
+chart_persentasi_pengangguran = alt.Chart(df).mark_line(point=True).encode(
+    x=alt.X("dates", title="Tahun", axis=alt.Axis(
+        format='%Y')),
+    y=alt.Y("Presentase pengangguran (%)", scale=alt.Scale(domain=[0, 9]))
+).properties(
+    title="Grafik Persentase Pengangguran (%)"
+)
+col1, col2 = st.columns(2)
+
+with col1:
+    st.altair_chart(chart_rata_rata_lama_sekolah, use_container_width=True)
+
+with col2:
+    st.altair_chart(chart_persentasi_pengangguran, use_container_width=True)
+
+# st.dataframe(new_df)
+
+st.write("Grafik diatas menunjukkan rata rata lama sekolah di indonesia cenderung stabil selama 9 tahun terakhir. Hal tersebut tercermin meningkatnya rata-rata lama sekolah di Indonesia dari tahun ke tahun. Data-data lama sekolah di Indonesia dari tahun 2013 hingga 2021 hanya meningkat sebanyak 0,70% dari awalnya 7,61 tahun ke 8,54 tahun. Sedangkan persentase pengangguran mengalami fluktuasi selama sembilan tahun terakhir. Tahun 2015 menjadi awal dari menurunnya persentase pengangguran hingga pada tahun 2019. Tingkat perubahan signifikan persentase pengangguran terjadi pada tahun 2020 hingga 2021 yang telah diketahui pada periode tersebut merupakan awal dari munculnya COVID-19.")
 
 
 st.subheader("Kesimpulan")
 st.markdown(
-    "1. Rata-rata lama sekolah di Indonesia tidak mempengaruhi persentase pengangguran di Indonesia.")
+    "1. Rata-rata lama sekolah di Indonesia memiliki korelasi rendah dengan persentase pengangguran di Indonesia.")
 st.markdown(
     "2. Rata-rata lama sekolah meningkat cenderung stabil dan tidak signifikan dari tahun ke tahun")
 st.markdown(
     "3. Persentase pengangguran meningkat secara signifikan saat munculnya wabah COVID-19 pada tahun 2020.")
 
-st.subheader("Rekomendasi")
-st.markdown(
-    "Pemerintah memberikan perhatian lebih untuk meningkatkan rata-rata lama sekolah di Indonesia, karena rata-rata lama sekolah di Indonesia tergolong rendah jika dibandingkan dengan negara lain.")
+# st.subheader("Rekomendasi")
+# st.markdown(
+#     "Pemerintah memberikan perhatian lebih untuk meningkatkan rata-rata lama sekolah di Indonesia, karena rata-rata lama sekolah di Indonesia tergolong rendah jika dibandingkan dengan negara lain.")
 
 st.subheader("Referensi")
 st.markdown('Penelitian')
